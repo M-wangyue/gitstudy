bdd19042bc3078313eed846eebafe2bbbe7e78ed
wangyue <2256162144@qq.com>
2021-07-04 10:00:03 +0800

    斐波拉契

---
+++ fblq.js
@@ -0,0 +1,11 @@
+function fblq(n){
+    var num1 = 1;
+    var num2 = 2;
+    var sum = num2;
+    for(var i =2;i < n;i++){
+        sum= num1+num2;
+        num1=num2;
+        num2=sum;
+    }
+    return sum;
+}
